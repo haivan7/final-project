39a1d195350cf026926c36559d1e799a07e48b70
haivan7 <129729329+haivan7@users.noreply.github.com>
2023-11-29 08:29:32 +0000

    nop bai final

--- Data/Data_cleaned/clear.py
+++ Data/Data_cleaned/clear.py
@@ -1,7 +1,9 @@
+# Import các thư viện cần thiết
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 # Đọc dữ liệu từ file CSV
-df = pd.read_csv('thehackernews2.csv')
+df = pd.read_csv('thehackernews10.csv')
 
 # Loại bỏ các dòng trùng lặp
 df = df.drop_duplicates()
@@ -13,5 +15,20 @@ df = df[df['post_text'].str.strip() != ""]
 df = df[pd.notnull(df['time'])]
 
 # Lưu dữ liệu đã làm sạch vào một file mới
-df.to_csv('thehackernews2_cleaned.csv', index=False)
+df.to_csv('thehackernews10_cleaned.csv', index=False)
 
+# Đọc dữ liệu đã làm sạch từ file CSV
+df = pd.read_csv('thehackernews10_cleaned.csv')
+
+# Chuyển đổi thời gian đăng từ chuỗi sang đối tượng datetime
+df['time'] = pd.to_datetime(df['time'])
+
+# Điền vào các giá trị bị thiếu
+# Ở đây, ta sẽ điền vào các giá trị bị thiếu bằng giá trị xuất hiện nhiều nhất
+for column in df.columns:
+    mode = df[column].mode()
+    if not mode.empty:
+        df[column].fillna(mode[0], inplace=True)
+
+# Lưu dữ liệu đã tiền xử lý vào một file mới
+df.to_csv('thehackernews10_preprocessed.csv', index=False)
